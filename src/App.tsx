e43521fd58e3f4ab3c0c06da7bbdbaf07e26aa85
Emma <emma.mccracken14@gmail.com>
2021-11-11 13:27:41 +0000

    tidy and add data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import "./babyNamesData";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <button className="buttonF">Abby</button>
+      <button className="buttonM">Andy</button>
+    </div>
+  );
 }
 
 export default App;
